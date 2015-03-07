f1a0eb8b2a2d9aaca73efc8e681d4a55eb3270f1
Lucius0 <451130764@qq.com>
2015-03-07 19:05:27 +0800

    can show but click

--- MatchGame/bin-debug/src/Card.js
+++ MatchGame/bin-debug/src/Card.js
@@ -1,14 +1,14 @@
-class Card extends egret.DisplayObjectContainer
-{
-    private flipStep:number;
-    private isFlipping:boolean = false;
-    private flipToFrame:number;
-    public cardFace:number;
-    private mc:egret.MovieClip;
-
-    public constructor()
-    {
-        super();
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var Card = (function (_super) {
+    __extends(Card, _super);
+    function Card() {
+        _super.call(this);
+        this.isFlipping = false;
         var data = RES.getRes("card_json");
         var texture = RES.getRes("card_png");
         var mcDataFactory = new egret.MovieClipDataFactory(data, texture);
@@ -16,36 +16,27 @@ class Card extends egret.DisplayObjectContainer
         //this.mc.gotoAndStop(1);
         this.addChild(this.mc);
     }
-
-    public startFlip(flipToWhichFrame:number):void
-    {
+    Card.prototype.startFlip = function (flipToWhichFrame) {
         this.isFlipping = true;
         this.flipStep = 10;
         this.flipToFrame = flipToWhichFrame;
         this.mc.addEventListener(egret.Event.ENTER_FRAME, this.flip, this);
-    }
-
-    public flip(event:egret.Event):void
-    {
+    };
+    Card.prototype.flip = function (event) {
         this.flipStep--;
-
-        if(this.flipStep > 5)
-        {
+        if (this.flipStep > 5) {
             this.mc.scaleX = .20 * (this.flipStep - 6);
         }
-        else
-        {
+        else {
             this.mc.scaleX = .20 * (5 - this.flipStep);
         }
-
-        if(this.flipStep == 5)
-        {
+        if (this.flipStep == 5) {
             this.mc.gotoAndStop(this.flipToFrame);
         }
-
-        if(this.flipStep == 0)
-        {
+        if (this.flipStep == 0) {
             this.mc.removeEventListener(egret.Event.ENTER_FRAME, this.flip, this);
         }
-    }
-}
+    };
+    return Card;
+})(egret.DisplayObjectContainer);
+Card.prototype.__class__ = "Card";
--- MatchGame/bin-debug/src/GameApp.js
+++ MatchGame/bin-debug/src/GameApp.js
@@ -1,76 +1,50 @@
-class GameApp extends egret.DisplayObjectContainer
-{
-    private static boardWidth:number = 6;
-    private static boardHeight:number = 6;
-    private static cardHorizontalSpacing:number = 52;
-    private static cardVerticalSpacing:number = 52;
-    private static boardOffsetX:number = 145;
-    private static boardOffsetY:number = 70;
-    private static pointsForMatch:number = 100;
-    private static pointsForMiss:number = -5;
-
-    private firstCard:Card;
-    private secondCard:Card;
-    private cardsLeft:number;
-    private gameScore:number;
-    private gameStartTime:number;
-    private gameTime:number;
-
-    private gameScoreField:egret.TextField;
-    private gameTimeField:egret.TextField;
-
-    private flipBackTimer:egret.Timer;
-
-    public constructor()
-    {
-        super();
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var GameApp = (function (_super) {
+    __extends(GameApp, _super);
+    function GameApp() {
+        _super.call(this);
         this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
     }
-
-    private onAddToStage(event: egret.Event)
-    {
+    GameApp.prototype.onAddToStage = function (event) {
         //初始化Resource资源加载库
         RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
         RES.loadConfig("resource/matchGame.json", "resource/");
-    }
-
+    };
     /**
      * 配置文件加载完成,开始预加载preload资源组。
      */
-    private onConfigComplete(event: RES.ResourceEvent): void {
+    GameApp.prototype.onConfigComplete = function (event) {
         RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
         RES.addEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
         RES.loadGroup("matchGame");
-    }
-
+    };
     /**
      * preload资源组加载完成
      */
-    private onResourceLoadComplete(event: RES.ResourceEvent): void {
+    GameApp.prototype.onResourceLoadComplete = function (event) {
         if (event.groupName == "matchGame") {
             RES.removeEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
             this.createScene();
         }
-    }
-
-    private createScene():void
-    {
-        var cardList:Array<number> = new Array();
-        for(var i:number = 0; i < GameApp.boardHeight * GameApp.boardWidth / 2; i++)
-        {
+    };
+    GameApp.prototype.createScene = function () {
+        var cardList = new Array();
+        for (var i = 0; i < GameApp.boardHeight * GameApp.boardWidth / 2; i++) {
             cardList.push(i);
             cardList.push(i);
         }
-
         this.cardsLeft = 0;
-        for(var x:number = 0; x < GameApp.boardWidth; x++)
-        {
-            for(var y:number = 0; y < GameApp.boardHeight; y++)
-            {
-                var c:Card = new Card();
+        for (var x = 0; x < GameApp.boardWidth; x++) {
+            for (var y = 0; y < GameApp.boardHeight; y++) {
+                var c = new Card();
                 c.x = x * GameApp.cardHorizontalSpacing + GameApp.boardOffsetX; // set position
                 c.y = y * GameApp.cardVerticalSpacing + GameApp.boardOffsetY;
-                var r:number = Math.floor(Math.random() * cardList.length);
+                var r = Math.floor(Math.random() * cardList.length);
                 c.cardFace = cardList[r];
                 cardList.splice(r, 1);
                 c.addEventListener(egret.TouchEvent.TOUCH_TAP, this.clickCard, this);
@@ -79,13 +53,11 @@ class GameApp extends egret.DisplayObjectContainer
                 this.cardsLeft++;
             }
         }
-
         this.gameScoreField = new egret.TextField();
         this.gameScoreField.textColor = 0x000000;
         this.addChild(this.gameScoreField);
         this.gameScore = 0;
         this.showGameScore();
-
         this.gameTimeField = new egret.TextField();
         this.gameTimeField.textColor = 0x000000;
         this.gameTimeField.x = 300;
@@ -93,47 +65,35 @@ class GameApp extends egret.DisplayObjectContainer
         this.gameStartTime = egret.getTimer();
         this.gameTime = 0;
         this.addEventListener(egret.Event.ENTER_FRAME, this.showTime, this);
-    }
-
-    private clickCard(e:egret.TouchEvent):void
-    {
-        var thisCard:Card = e.target;
+    };
+    GameApp.prototype.clickCard = function (e) {
+        var thisCard = e.target;
         console.log(thisCard);
-        if(this.firstCard == null)
-        {
+        if (this.firstCard == null) {
             this.firstCard = thisCard;
-            var temp:number = thisCard.cardFace + 2;
+            var temp = thisCard.cardFace + 2;
             thisCard.startFlip(temp);
         }
-        else if(this.firstCard == thisCard)
-        {
+        else if (this.firstCard == thisCard) {
             this.firstCard.startFlip(1);
             this.firstCard == null;
         }
-        else if(this.secondCard == null)
-        {
+        else if (this.secondCard == null) {
             this.secondCard = thisCard;
-            var temp:number = thisCard.cardFace + 2;
+            var temp = thisCard.cardFace + 2;
             thisCard.startFlip(temp);
-
-            if(this.firstCard.cardFace == this.secondCard.cardFace)
-            {
+            if (this.firstCard.cardFace == this.secondCard.cardFace) {
                 this.removeChild(this.firstCard);
                 this.removeChild(this.secondCard);
-
                 this.firstCard = null;
                 this.secondCard = null;
-
                 this.gameScore += GameApp.pointsForMatch;
                 this.showGameScore();
-
-                if(this.cardsLeft == 0)
-                {
+                if (this.cardsLeft == 0) {
                     console.log("game over");
                 }
             }
-            else
-            {
+            else {
                 this.gameScore += GameApp.pointsForMiss;
                 this.showGameScore();
                 this.flipBackTimer = new egret.Timer(2000, 1);
@@ -141,43 +101,42 @@ class GameApp extends egret.DisplayObjectContainer
                 this.flipBackTimer.start();
             }
         }
-        else
-        {
+        else {
             this.returnCards(null);
-
             this.firstCard = thisCard;
-            var temp:number = thisCard.cardFace + 2;
+            var temp = thisCard.cardFace + 2;
             this.firstCard.startFlip(temp);
         }
-    }
-
-    private returnCards(e:egret.TimerEvent):void
-    {
+    };
+    GameApp.prototype.returnCards = function (e) {
         this.firstCard.startFlip(1);
         this.secondCard.startFlip(1);
         this.firstCard = null;
         this.secondCard = null;
         this.flipBackTimer.removeEventListener(egret.TimerEvent.TIMER_COMPLETE, this.returnCards, this);
-
-    }
-
-    private showTime(event:Event):void
-    {
+    };
+    GameApp.prototype.showTime = function (event) {
         this.gameTime = egret.getTimer() - this.gameStartTime;
         this.gameTimeField.text = "Time:  " + this.clockTime(this.gameTime);
-    }
-
-    private clockTime(ms:number):string
-    {
-        var seconds:number = Math.floor(ms/1000);
-        var minutes:number = Math.floor(seconds / 60);
+    };
+    GameApp.prototype.clockTime = function (ms) {
+        var seconds = Math.floor(ms / 1000);
+        var minutes = Math.floor(seconds / 60);
         seconds -= minutes * 60;
-        var timeString:string = minutes + ":" + String(seconds + 100).substr(1, 2);
+        var timeString = minutes + ":" + String(seconds + 100).substr(1, 2);
         return timeString;
-    }
-
-    private showGameScore():void
-    {
+    };
+    GameApp.prototype.showGameScore = function () {
         this.gameScoreField.text = "Score:  " + String(this.gameScore);
-    }
-}
+    };
+    GameApp.boardWidth = 6;
+    GameApp.boardHeight = 6;
+    GameApp.cardHorizontalSpacing = 52;
+    GameApp.cardVerticalSpacing = 52;
+    GameApp.boardOffsetX = 145;
+    GameApp.boardOffsetY = 70;
+    GameApp.pointsForMatch = 100;
+    GameApp.pointsForMiss = -5;
+    return GameApp;
+})(egret.DisplayObjectContainer);
+GameApp.prototype.__class__ = "GameApp";
